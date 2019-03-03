14c4ba7b1cff82d32c4af9467286acc5adfe4a45
JM <woalsdl600@gmail.com>
2019-03-03 19:07:23 +0900

    UPDATE DQN

--- DQN.py
+++ DQN.py
@@ -7,40 +7,67 @@ Created on Sat Mar  2 17:00:27 2019
 import numpy as np
 import tensorflow as tf
 
+
 class DQN:
-    def __init__(self,session,input_size,output_size,name='main'):
+
+    def __init__(self, session: tf.Session, input_size: int, output_size: int, name: str="main") -> None:
+        """DQN Agent can
+        1) Build network
+        2) Predict Q_value given state
+        3) Train parameters
+        Args:
+            session (tf.Session): Tensorflow session
+            input_size (int): Input dimension
+            output_size (int): Number of discrete actions
+            name (str, optional): TF Graph will be built under this name scope
+        """
         self.session = session
         self.input_size = input_size
         self.output_size = output_size
         self.net_name = name
-        
-        self._build_network()
-        
-    def _build_network(self,h_size=10,learning_rate=0.1):
-        with tf.variable_scope(self.net_name):
-            self.X = tf.placeholder(tf.float32,[None,self.input_size],name='input_x')
-            
-            W1 = tf.get_variable("W1",shape=[self.input_size,h_size],
-                                 initialize=tf.contrib.layers.xavier_initializer())
-            
-            layer1 = tf.nn.tanh(tf.matmul(self.X,W1))
-            
-            W2 = tf.get_variable("W2",shape=[self.h_size,output_size],
-                                 initialize=tf.contrib.layers.xavier_initializer())
 
-            self.Qpred = tf.matmul(layer1,W2)
-            
-        self.Y = tf.placeholder(shape=[None,self.output_size],dtype=tf.float32)
-        
-        self.loss = tf.reduce_mean(tf.square(self.Qpred - self.Y))
-        
-        self.train = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(self.loss)
-        
-    def predict(self,state):
-        x = np.reshape(state,[1,self.input_size])
-        return self.session.run(self.Qpred, feed_dict={self.X: x})
-    
-    def update(self,x_stack,y_stack):
-        return self.session.run(self.loss,self.train,feed_dict={self.X: x_stack, self.Y: y_stack})
-    
-     
+        self._build_network()
+
+    def _build_network(self, h_size=16, l_rate=0.001) -> None:
+        """DQN Network architecture (simple MLP)
+        Args:
+            h_size (int, optional): Hidden layer dimension
+            l_rate (float, optional): Learning rate
+        """
+        with tf.variable_scope(self.net_name):
+            self._X = tf.placeholder(tf.float32, [None, self.input_size], name="input_x")
+            net = self._X
+
+            net = tf.layers.dense(net, h_size, activation=tf.nn.relu)
+            net = tf.layers.dense(net, self.output_size)
+            self._Qpred = net
+
+            self._Y = tf.placeholder(tf.float32, shape=[None, self.output_size])
+            self._loss = tf.losses.mean_squared_error(self._Y, self._Qpred)
+
+            optimizer = tf.train.AdamOptimizer(learning_rate=l_rate)
+            self._train = optimizer.minimize(self._loss)
+
+    def predict(self, state: np.ndarray) -> np.ndarray:
+        """Returns Q(s, a)
+        Args:
+            state (np.ndarray): State array, shape (n, input_dim)
+        Returns:
+            np.ndarray: Q value array, shape (n, output_dim)
+        """
+        x = np.reshape(state, [-1, self.input_size])
+        return self.session.run(self._Qpred, feed_dict={self._X: x})
+
+    def update(self, x_stack: np.ndarray, y_stack: np.ndarray) -> list:
+        """Performs updates on given X and y and returns a result
+        Args:
+            x_stack (np.ndarray): State array, shape (n, input_dim)
+            y_stack (np.ndarray): Target Q array, shape (n, output_dim)
+        Returns:
+            list: First element is loss, second element is a result from train step
+        """
+        feed = {
+            self._X: x_stack,
+            self._Y: y_stack
+        }
+        return self.session.run([self._loss, self._train], feed)
